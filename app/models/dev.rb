b4ecd06951959dc5e7ea7ce90f071c7929d57e43
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-15 13:48:11 -0700

    initial commit

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,7 +3,7 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
 
     def received_one?(item_name)
-        !!self.freebies.filter {|freebie| freebie.item_name = item_name}
+        self.freebies.filter {|freebie| freebie.item_name == item_name} == [] ? false : true
     end
 
 end
