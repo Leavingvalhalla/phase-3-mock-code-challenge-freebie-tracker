74d57391c7c3e89fde6c2a072fc4dd54a92fca57
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-15 13:26:42 -0700

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,17 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Dev.create(name: "Rick")
+Dev.create(name: "Morty")
+Dev.create(name: "Mr. Meseeks")
+Dev.create(name: "Gazorpazop")
+
+10.times do
+Freebie.create(item_name: Faker::Kpop.girl_groups, 
+    value: rand(1...100),
+    company_id: rand(1..4),
+    dev_id: rand(1..4))
+end
+
 
 puts "Seeding done!"
